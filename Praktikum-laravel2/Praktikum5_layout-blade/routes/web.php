89ee0eb02dd96d628bfe3237ec37566db8b284d0
fikagithub <fikaadilah24@gmail.com>
2022-03-23 14:45:19 +0700

    Pratikum_2020573010023

---
+++ Praktikum-laravel2/Praktikum5_layout-blade/routes/web.php
@@ -0,0 +1,67 @@
+<?php
+
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+//Route Bawaan Laravel
+Route::get('/', function () {
+    return view('welcome');
+});
+
+//Blade Template Engine
+Route::get('/mahasiswa', function(){
+    $nama='Fika Adilah';
+    $nilai=75;
+    return view('mahasiswa',compact('nama','nilai'));
+});
+
+Route::get('/mahasiswa', function(){
+    $nama='<u>Fika Adilah</u>';
+    $nilai=75;
+    return view('mahasiswa',compact('nama','nilai'));
+});
+
+//Kondisi If Else
+Route::get('/mahasiswa', function(){
+     $nama='Fika Adilah';
+     $nilai=105;
+     return view('mahasiswa',compact('nama','nilai'));
+});
+
+//Kondisi Switch
+Route::get('/mahasiswa', function(){
+    $nama='Fika Adilah';
+    $nilai=95;
+    return view('mahasiswa',compact('nama','nilai'));
+});
+
+//Perulangan Foreach
+Route::get('/mahasiswa', function(){
+    $nama='Fika Adilah';
+    $nilai=[80,64,30,76,95];
+    return view('mahasiswa',compact('nama','nilai'));
+});
+
+//Perulangan Forelse
+Route::get('/mahasiswa', function(){
+    $nama='Fika Adilah';
+    $nilai=[];
+    return view('mahasiswa',compact('nama','nilai'));
+});
+
+//Perintah Continue dan Break
+Route::get('/mahasiswa', function(){
+    $nama = 'Fika Adilah';
+    $nilai = [80,64,30,76,95];
+    return view('mahasiswa',compact('nama','nilai'));
+});
