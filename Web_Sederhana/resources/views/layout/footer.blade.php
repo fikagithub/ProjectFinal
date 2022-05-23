9ae37462c2dac1cdc9946e102dbc314981a04bd8
fikagithub <fikaadilah24@gmail.com>
2022-05-23 11:29:38 +0700

    Praktikum_Laravel_2020573010023

--- Web_Sederhana/resources/views/layout/footer.blade.php
+++
@@ -1,7 +0,0 @@
-<footer class="bg-red py-2 text-white mt-4">
-    <div class="container">
-        Sistem Halaman Penjualan | Copyright &copy; {{date("Y")}} BY_MOODD
-    </div>
-</footer>
-</body>
-</html>
